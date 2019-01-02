88564efdb629c524e4616fd1c081498297ef8e59
javaterminology <vrajasekharreddy@gmail.com>
2019-01-03 00:56:10 +0800

    just-gif-it

---
+++ just-gif-it/src/main/java/com/raja/justgifit/JustGifItApplication.java
@@ -0,0 +1,14 @@
+package com.raja.justgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifItApplication.class, args);
+	}
+
+}
+
