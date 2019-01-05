f0e2f03cf638b55d0556f17060d626612b8674a2
javaterminology <vrajasekharreddy@gmail.com>
2019-01-05 23:27:16 +0800

    commit changes

--- just-gif-it-autoconfigure/src/main/java/com/raja/justgifit/JustGifItAutoConfiguration.java
+++ just-gif-it-autoconfigure/src/main/java/com/raja/justgifit/JustGifItAutoConfiguration.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JustGifItAutoConfiguration {
 	public static void main(String[] args) {
 		SpringApplication.run(JustGifItAutoConfiguration.class, args);
+		////////////////
 	}
 }
